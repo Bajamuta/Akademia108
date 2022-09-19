6d9b7439f27ccec38ac39d041e37b7f3ff28a50b
jprajzendanc <j.prajzendanc@gmail.com>
2022-09-19 09:49:59 +0200

    zadanie 32-js-wieksza-liczba

---
+++ 32-js-wieksza-liczba/js/main.js
@@ -0,0 +1,12 @@
+let num1 = 2;
+let num2 = 5;
+
+function suma(l1, l2) {
+    let temp = l1;
+    if (l2 > l1) {
+        temp = l2;
+    }
+    return temp;
+}
+
+console.log('suma:', suma(num1, num2));
