675c5793dcd2febebfa3b522cb5d9dff9425e723
jprajzendanc <j.prajzendanc@gmail.com>
2022-10-24 11:23:20 +0200

    3-pobierz-dane-po-kliknieciu-przycisku-jquery

---
+++ 3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+   const btn = document.createElement("button");
+   btn.innerText = "POBIERZ DANE";
+   $("body").append(btn);
+   $("button").click(() => {
+      $.get("https://akademia108.pl/api/ajax/get-post.php")
+          .then(data => {
+             console.log(data);
+             $("#main").append(`<h1>get()</h1>`);
+             $("#main").append(`<p>Post id: ${data.id}</p>`);
+             $("#main").append(`<h2>${data.title}</h2>`);
+             $("#main").append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
+          })
+          .catch(error => console.error(JSON.stringify(error)));
+      $.getJSON("https://akademia108.pl/api/ajax/get-post.php")
+          .then(data => {
+             console.log(data);
+             $("#main").append(`<h1>getJSON()</h1>`);
+             $("#main").append(`<p>Post id: ${data.id}</p>`);
+             $("#main").append(`<h2>${data.title}</h2>`);
+             $("#main").append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
+          })
+          .catch(error => console.error(JSON.stringify(error)));
+   });
+});
