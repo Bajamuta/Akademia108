0bf70451e47cc9b4d30294c325eb704f9a9d8eec
jprajzendanc <j.prajzendanc@gmail.com>
2022-10-24 11:25:28 +0200

    3-pobierz-dane-po-kliknieciu-przycisku-jquery: poprawka nazw funkcji

--- 3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/main.js
+++ 3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/main.js
@@ -4,22 +4,22 @@ $(document).ready(function () {
    $("body").append(btn);
    $("button").click(() => {
       $.get("https://akademia108.pl/api/ajax/get-post.php")
-          .then(data => {
+          .done(data => {
              console.log(data);
              $("#main").append(`<h1>get()</h1>`);
              $("#main").append(`<p>Post id: ${data.id}</p>`);
              $("#main").append(`<h2>${data.title}</h2>`);
              $("#main").append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
           })
-          .catch(error => console.error(JSON.stringify(error)));
+          .fail(error => console.error(JSON.stringify(error)));
       $.getJSON("https://akademia108.pl/api/ajax/get-post.php")
-          .then(data => {
+          .done(data => {
              console.log(data);
              $("#main").append(`<h1>getJSON()</h1>`);
              $("#main").append(`<p>Post id: ${data.id}</p>`);
              $("#main").append(`<h2>${data.title}</h2>`);
              $("#main").append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
           })
-          .catch(error => console.error(JSON.stringify(error)));
+          .fail(error => console.error(JSON.stringify(error)));
    });
 });
