024cc49d1cd58a62bae44aa3eeb249cbeb96ba03
jprajzendanc <j.prajzendanc@gmail.com>
2022-10-24 11:27:04 +0200

    3-pobierz-dane-po-kliknieciu-przycisku-jquery: optymalizacja zmiennych

--- 3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/main.js
+++ 3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/main.js
@@ -6,19 +6,21 @@ $(document).ready(function () {
       $.get("https://akademia108.pl/api/ajax/get-post.php")
           .done(data => {
              console.log(data);
-             $("#main").append(`<h1>get()</h1>`);
-             $("#main").append(`<p>Post id: ${data.id}</p>`);
-             $("#main").append(`<h2>${data.title}</h2>`);
-             $("#main").append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
+             const main = $("#main");
+             main.append(`<h1>get()</h1>`);
+             main.append(`<p>Post id: ${data.id}</p>`);
+             main.append(`<h2>${data.title}</h2>`);
+             main.append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
           })
           .fail(error => console.error(JSON.stringify(error)));
       $.getJSON("https://akademia108.pl/api/ajax/get-post.php")
           .done(data => {
              console.log(data);
-             $("#main").append(`<h1>getJSON()</h1>`);
-             $("#main").append(`<p>Post id: ${data.id}</p>`);
-             $("#main").append(`<h2>${data.title}</h2>`);
-             $("#main").append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
+             const main = $("#main");
+             main.append(`<h1>getJSON()</h1>`);
+             main.append(`<p>Post id: ${data.id}</p>`);
+             main.append(`<h2>${data.title}</h2>`);
+             main.append(`<article><p>${data.body}</p><p class="author">Created by: ${data.userId}</p></article>`);
           })
           .fail(error => console.error(JSON.stringify(error)));
    });
