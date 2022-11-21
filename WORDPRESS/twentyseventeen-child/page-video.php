a87f865d2485babd6fdbb74405fcd6934e8a7207
jprajzendanc <j.prajzendanc@gmail.com>
2022-11-21 09:22:37 +0100

    WORDPRESS - brakujący fragment z filmikiem

--- WORDPRESS/twentyseventeen-child/page-video.php
+++ WORDPRESS/twentyseventeen-child/page-video.php
@@ -5,6 +5,21 @@
 
 get_header();
 
+?>
+
+<div class="wrap">
+
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/tgbNymZ7vqY">
+</iframe>
+
+<?php
+the_content();
+?>
+
+</div>
+
+<?php
 get_footer();
 
 ?>
