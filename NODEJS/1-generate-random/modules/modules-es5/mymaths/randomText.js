8a59de3841aef72fa57693df8d66adb73844ccd1
jprajzendanc <j.prajzendanc@gmail.com>
2023-01-12 14:44:23 +0100

    working on random string generator

--- NODEJS/1-generate-random/modules/modules-es5/mymaths/randomText.js
+++ NODEJS/1-generate-random/modules/modules-es5/mymaths/randomText.js
@@ -1,4 +1,10 @@
 function randomText(length) {
- return 'TEST';
+    let result = '';
+    Array.from(Array(length), () => Math.floor(Math.random() * 36).toString(36)).join('');
+    for (let i = 0; i < length; i ++)
+    {
+        result += Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+    }
+    return result;
 }
 module.exports = randomText;
