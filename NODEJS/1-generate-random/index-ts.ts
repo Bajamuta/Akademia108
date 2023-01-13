18d1c82e12d8d31fe20ab0eba5c345b835e2f2fe
jprajzendanc <j.prajzendanc@gmail.com>
2023-01-13 11:54:32 +0100

    working on random array

--- NODEJS/1-generate-random/index-ts.ts
+++ NODEJS/1-generate-random/index-ts.ts
@@ -2,6 +2,6 @@ import mymaths from "./modules/modules-es5/mymaths";
 let minimum = parseInt(process.argv[2]);
 let maximum = parseInt(process.argv[3]);
 let length = parseInt(process.argv[4]);
-console.log('Your results in typescript:', mymaths.randomNumber(minimum, maximum));
+console.log('Your random number in typescript:', mymaths.randomNumber(minimum, maximum));
 console.log('Your random string in typescript:', mymaths.randomText(length));
 console.log('Your random array in typescript:', mymaths.randomArray(minimum, maximum, length));
