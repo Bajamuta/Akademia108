20249471b7d54b587f95e009f98816e63912abed
jprajzendanc <j.prajzendanc@gmail.com>
2023-01-23 13:04:44 +0100

    node modules inside project directory; proper hierarchy and paths to views & partials

--- NODEJS/1-express/index.js
+++ NODEJS/1-express/index.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const app = express();
+const hbs = require('express-handlebars');
+const path = require("path");
 
 /*
 app.get('/', (req, res) => {
     res.send('Witaj programisto!');
 });
-
 app.get('/user/:id', (req, res) => {
    res.send('Tutaj user id: ' + req.params.id);
 });
-
 app.get('/user/:id?/:name?', (req, res) => {
     res.send('Tutaj user opcjonalny: ' + req.params.id);
 });
-
 // QUERY
 app.get('/data', (req, res) => {
     res.send('Tutaj query: ' + req.query.search + ' ' + req.query.model);
 });
 */
 
-/*app.get('/', (req, res) => {
-    res.send('Witaj programisto!');
-});*/
-
 // we can add prefix to our path
-app.use('/test', express.static(__dirname + '/public'));
+// app.use('/test', express.static('/public'));
+app.use(express.static('/public'));
+
+// views and view engine
+app.engine("hbs", hbs.engine({extname: 'hbs', defaultLayout: 'main'}));
+app.set("view engine", "hbs");
+
+// IMPORTANT + file hierarchy!
+app.set("views", path.join(__dirname, "views"));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen(8080, () => console.log('Serwer Node.Js działa'));
 
-// uruchamianie nodemon: npm start lub npx nodemon
+// to run nodemon: npm start (need to configure) or npx nodemon PATH_TO/index.js
