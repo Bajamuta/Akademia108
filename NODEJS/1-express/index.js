802d085d637cc21af93723ffd17fdd8b6caa301b
jprajzendanc <j.prajzendanc@gmail.com>
2023-01-23 11:18:08 +0100

    folder public

--- NODEJS/1-express/index.js
+++ NODEJS/1-express/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
 
+/*
 app.get('/', (req, res) => {
-    res.send('Witaj programisto!')
+    res.send('Witaj programisto!');
 });
+
+app.get('/user/:id', (req, res) => {
+   res.send('Tutaj user id: ' + req.params.id);
+});
+
+app.get('/user/:id?/:name?', (req, res) => {
+    res.send('Tutaj user opcjonalny: ' + req.params.id);
+});
+
+// QUERY
+app.get('/data', (req, res) => {
+    res.send('Tutaj query: ' + req.query.search + ' ' + req.query.model);
+});
+*/
+
+/*app.get('/', (req, res) => {
+    res.send('Witaj programisto!');
+});*/
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, () => console.log('Serwer Node.Js działa'));
 
